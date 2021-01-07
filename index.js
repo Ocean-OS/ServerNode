ebc9c30c4809c3215cbdbdf104b3c03a8b92320d
DREWNOLT <63362464+DREWNOLT@users.noreply.github.com>
2021-01-07 14:21:54 -0800

    Add SendHTMLcontent function
    Added function for sending bare HTML content put in the server script, instead of from a file

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const port = 3000
 //Examples are below, please delete unless you create the files "index.html" and "error.html"
 createWebPageFor('/', '/index.html');
 sendErrorFile('/error.html');
+sendHTMLcontentfor('/html', '<title>HTML</title>')
 
 //The "sendErrorFile(file)" function asks for what file to show when an error occurs, and the "createWebPageFor(path, fileSent)" function sends out files at request when you put the path and the file in the function.
 
 function createWebPageFor(path, fileSent){
   app.get(path, (request, response) => {
-    request.console.log("Sending File for request");
     response.sendFile(__dirname + fileSent);
   });
 }
@@ -23,5 +23,11 @@ function sendErrorFile(file){
     return console.log('something bad happened', err)
   };
    console.log(`server is listening on ${port}`);
+  });
+}
+
+function sendHTMLcontentfor(path, html){
+  app.get(path, (request, response) => {
+    response.send(html);
   });
 }
